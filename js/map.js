f5d90ab1c05aee168967b0bcb29f804f35a3ca4a
Dror Bogin <Dror.Bogin@gmail.com>
2018-05-20 11:54:41 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -6,7 +6,7 @@
 				var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 				attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 				});
-				var rbm = L.tileLayer('file://tiles/{z}/{x}/{y}.png');
+				var rbm = L.tileLayer('/tiles/{z}/{x}/{y}.png');
 				
 				
 				
