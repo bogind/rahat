aec5f84dfb3bbcfd7dd5aec5f777f02ba373d62f
Dror Bogin <Dror.Bogin@gmail.com>
2018-05-20 11:58:02 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -2,11 +2,11 @@
 				
 				// Add OpenStreetMap tile layer variables
 				// need to check about creating a local tile server
-				var OSM = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',{attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'});
+				var OSM = L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png',{attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'});
 				var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 				attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 				});
-				var rbm = L.tileLayer('/tiles/{z}/{x}/{y}.png');
+				var rbm = L.tileLayer('https://github.com/bogind/rahat/blob/master/tiles/{z}/{x}/{y}.png');
 				
 				
 				
